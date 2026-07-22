295766973e8ca59407b3f668ce1a4df278574e8b
Mohammad Robiul Hossen <robiul99hossen@gmail.com>
2026-07-22 16:44:46 +0600

    fix: build error fixed on register page

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,5 +1,17 @@
+import { Suspense } from "react";
 import Register from "@/components/Register/Register";
+import { Spinner } from "@/components/ui/spinner";
 
 export default function RegisterPage() {
-  return <Register />;
+  return (
+    <Suspense
+      fallback={
+        <>
+          <Spinner />
+        </>
+      }
+    >
+      <Register />;
+    </Suspense>
+  );
 }
